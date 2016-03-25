97faa14ebf32fd2a82252f04c3805f6ff70dae48
SigvartBrendberg <walterhohman@gmail.com>
2016-03-25 14:58:10 +0100

    added JS support for elevator mass calculation

--- js/tether.js
+++ js/tether.js
@@ -7,6 +7,14 @@ moonGparameter = 4904869590000; //G*M
 earthGparameter = 398600441890000; //G*m
 EMdistance = 384400000;
 eml1DistanceFromTheMoon = 58730214;
+integralCallInitializer = function(start,end){
+  var SCALEstart = start/EMdistance;
+  var SCALEend = end/EMdistance;
+  var jStMaSsRaTiO = moonGparameter/(moonGparameter + earthGparameter);
+  var firstPoint = integralCall(SCALEstart,jStMaSsRaTiO);
+  var secondPoint = integralCall(SCALEend,jStMaSsRaTiO);
+  return (secondPoint - firstPoint) * (earthGparameter + moonGparameter)/EMdistance;
+};
 tether = function(){
   var surfacecheck = document.getElementById("surface").checked;
   var eml1check = document.getElementById("eml1").checked;
@@ -37,10 +45,23 @@ tether = function(){
   var jStMaSsRaTiO = moonGparameter/(moonGparameter + earthGparameter);
   var firstPoint = integralCall(SCALEstart,jStMaSsRaTiO);
   var secondPoint = integralCall(SCALEend,jStMaSsRaTiO);
-  var resultLE = (secondPoint - firstPoint) * (earthGparameter + moonGparameter)/EMdistance;
+  var resultLE = integralCallInitializer(startaltitude,endaltitude);
   var resultLE2 = Math.pow(Math.E,resultLE/strength);
   resultLE = Math.round(resultLE*100)/100;
   resultLE2 = Math.round(resultLE2*1000)/1000;
   document.getElementById("resultLE").innerHTML = "Acceleration potential: <b>"+resultLE+"</b> Yuri";
   document.getElementById("resultLE2").innerHTML = "Taper ratio: <b>"+resultLE2+"</b>";
+    if(masscheck){
+    var iter = 10000 //change to taste
+    var step = (endaltitude - startaltitude)/iter;
+    var payloadMass = Number(document.getElementById("payloadmass").value);
+    var safety = Number(document.getElementById("safety").value);
+    var maxLoadCrossSectionDensity = safety*payloadMass*moonGparameter/(startaltitude * startaltitude * strength);
+    var resultLE3 = 0;
+    for(i = startaltitude; i < endaltitude; i += step){
+      resultLE3 += step * maxLoadCrossSectionDensity * Math.pow(Math.E,integralCallInitializer(startaltitude,i)/strength);
+    };
+    resultLE3 = Math.round(resultLE3*100)/100;
+    document.getElementById("resultLE3").innerHTML = "Elevator mass: <b>"+resultLE3+" kg</b>";
+  };
 };
