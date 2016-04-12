01beaecf0396b2b64e6bb15e50df3b739a5dff6c
SigvartBrendberg <walterhohman@gmail.com>
2016-04-12 10:48:18 +0200

    transfer timing mechanics

--- browsergame/stuff.js
+++ browsergame/stuff.js
@@ -12,6 +12,8 @@ engines = [
 places = [
 //["id","display",[["con1","time","instant","time","spiral"],["con2","time","instant","time","spiral"]],"time","new"]
 ["LEO","LEO",[["LTOlow",0,3120],[]]],
-[],
-[]
+["LTOlow","LTO",[["LEO",0,3120]],100,"LTOmiddle"],
+["LTOmiddle","LTO",[],400,"LTOhigh"],
+["LTOhigh","LTO",[["Lescape",0,140]],200,"LTOreturn"],
+["LTOreturn","Ops!",[],1400,"LTOlow"]
 ];
